d35bcff5a3ddbb2bb2835dc63d2d7e9c81b43aa0
Boris <76632047+Breazzz@users.noreply.github.com>
2022-04-08 15:40:40 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,193 @@
+import React from "react";
+import { Formik } from "formik";
+import * as yup from "yup";
+
+// Валидация пароля
+// ----------------
+// password: yup.string()
+//   .typeError('Должно быть строкой') // Не обязательная строка
+//   .test('', '', () => { /* Дополнительная функция для проверки пароля */ })
+//   .matches(REG_EXP, 'Не соответствует правилу') // Проверка на соответствие регулярному выражению.
+//   .required('Обязательно'),
+// confirmPassword: yup.string()
+//   .oneOf([yup.ref('password')], 'Пароли не совпадают')
+//   .required('Обязательно')
+
+// Валидация email
+// ---------------
+// email: yup.string()
+//  .matches(REG_EXP, 'Не соответствует правилу') // Проверка на соответствие регулярному выражению.
+//   .email('Введите верный email').required('Обязательно'),
+
+function App() {
+  const validationsSchema = yup.object().shape({
+    name: yup.string().typeError("Должно быть строкой").required("Обязательно"),
+    secondName: yup
+      .string()
+      .typeError("Должно быть строкой")
+      .required("Обязательно"),
+    password: yup
+      .string()
+      .typeError("Должно быть строкой")
+      .required("Обязательно"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "Пароли не совпадают")
+      .required("Обязательно"),
+    email: yup.string().email("Введите верный email").required("Обязательно"),
+    confirmEmail: yup
+      .string()
+      .email("Введите верный email")
+      .oneOf([yup.ref("email")], "Email не совпадают")
+      .required("Обязательно")
+  });
+
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          name: "",
+          secondName: "",
+          password: "",
+          confirmPassword: "",
+          email: "",
+          confirmEmail: ""
+        }}
+        validateOnBlur
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={validationsSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          isValid,
+          handleSubmit,
+          dirty
+        }) => (
+          <div className={`from`}>
+            <p>
+              <label htmlFor={`name`}>Имя</label>
+              <br />
+              <input
+                className={"input"}
+                type={`text`}
+                name={`name`}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+              />
+            </p>
+            {touched.name && errors.name && (
+              <p style={{ color: "red" }} className={"error"}>
+                {errors.name}
+              </p>
+            )}
+            <p>
+              <label htmlFor={`secondName`}>Фамилия</label>
+              <br />
+              <input
+                className={"input"}
+                type={`text`}
+                name={`secondName`}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.secondName}
+              />
+            </p>
+            {touched.secondName && errors.secondName && (
+              <p style={{ color: "red" }} className={"error"}>
+                {errors.secondName}
+              </p>
+            )}
+            <p>
+              <label htmlFor={`secondName`}>Пароль</label>
+              <br />
+              <input
+                className={"input"}
+                type={`password`}
+                name={`password`}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+            </p>
+            {touched.password && errors.password && (
+              <p style={{ color: "red" }} className={"error"}>
+                {errors.password}
+              </p>
+            )}
+
+            <p>
+              <label htmlFor={`confirmPassword`}>Подтвердите пароль</label>
+              <br />
+              <input
+                className={"input"}
+                type={`password`}
+                name={`confirmPassword`}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.confirmPassword}
+              />
+            </p>
+            {touched.confirmPassword && errors.confirmPassword && (
+              <p style={{ color: "red" }} className={"error"}>
+                {errors.confirmPassword}
+              </p>
+            )}
+
+            <p>
+              <label htmlFor={`email`}>Email</label>
+              <br />
+              <input
+                className={"input"}
+                type={`email`}
+                name={`email`}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+            </p>
+            {touched.email && errors.email && (
+              <p style={{ color: "red" }} className={"error"}>
+                {errors.email}
+              </p>
+            )}
+
+            <p>
+              <label htmlFor={`confirmEmail`}>Подтвердите email</label>
+              <br />
+              <input
+                className={"input"}
+                type={`email`}
+                name={`confirmEmail`}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.confirmEmail}
+              />
+            </p>
+            {touched.confirmEmail && errors.confirmEmail && (
+              <p style={{ color: "red" }} className={"error"}>
+                {errors.confirmEmail}
+              </p>
+            )}
+
+            <button
+              disabled={!isValid || !dirty}
+              onClick={handleSubmit}
+              type={`submit`}
+            >
+              Отправить
+            </button>
+          </div>
+        )}
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
